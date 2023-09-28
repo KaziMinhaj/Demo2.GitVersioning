d11cf0769c9b423f4596880bdbd79c9c21899f74
Kazi Md Minhajul Haider <kaziminhajulhaider@gmail.com>
2023-09-28 17:50:25 +0600

    only merged codes

--- Demo2.GitVersioning/auth.js
+++ Demo2.GitVersioning/auth.js
@@ -1,3 +1,5 @@
 console.log('authentication added');
 console.log('another authentication added');
 console.log('major changed');
+console.log('another authentication added');
+console.log('major changed');
